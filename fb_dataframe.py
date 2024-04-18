2c7dd82d244acec9727a150d9360ca69ea07d45b
Ashwath Krishnan <ashwathkrishnan@Ashwaths-MacBook-Air.local>
2024-04-18 13:11:15 -0500

    groupby

--- fb_dataframe.py
+++ fb_dataframe.py
@@ -202,6 +202,7 @@ def fb_dataframe_group_by_sum(fb_bytes: bytes, grouping_col_name: str, sum_col_n
     sorted_groups = sorted(group_sums.items())
     result_df = pd.DataFrame(sorted_groups, columns=[grouping_col_name, sum_col_name])
     result_df.set_index(grouping_col_name, inplace=True)
+    return result_df
 
 
 def fb_dataframe_map_numeric_column(fb_buf: memoryview, col_name: str, map_func: types.FunctionType) -> None:
